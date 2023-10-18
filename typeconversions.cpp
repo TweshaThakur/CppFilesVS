e12eddbad1e491f08a1fe6b89505de03e5f499b8
Twesha <tweshathakur@gmail.com>
2023-10-18 10:23:02 +0530

    basic to basic basic to class

--- typeconversions.cpp
+++ typeconversions.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 using namespace std;
 
+class Time{
+    int hrs;
+    public:
+    Time(int t){
+        hrs=t/60;
+        cout<<hrs<<endl;
+    }
+};
 int main(){
     int x;
     float y=100.0;
     x=y;
     cout<<x;
     cout<<endl;
+
+    Time t1=85;
+    Time t2=120;
 }
