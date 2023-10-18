58894d2527f828cfa1f4f0acf261ff18a55cfd34
Twesha <tweshathakur@gmail.com>
2023-10-18 10:40:40 +0530

    added class to basic conversion

--- typeconversions.cpp
+++ typeconversions.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 using namespace std;
 
-class Time{
+class student{
+    private:
+    int rollno;
+    float fees;
+
+    public:
+    student(int a , float m){
+        rollno=a;
+        fees=m;
+    }
+    operator int(){return rollno;}
+    operator float(){return fees;}
+};
+int main(){
+    int j;
+    float f;
+    student st(50,45.67);
+
+    j=st;
+    f=st;
+
+    cout<<"Value of j "<<j<<endl;
+    cout<<"Value of f "<<f<<endl;
+}
+/**class Time{
     int hrs;
     public:
     Time(int t){
@@ -10,12 +34,17 @@ class Time{
     }
 };
 int main(){
+    //basic to basic
     int x;
     float y=100.0;
     x=y;
     cout<<x;
     cout<<endl;
-
+    //basic to class using constructor
     Time t1=85;
     Time t2=120;
-}
+
+    //class type to basic type using casting opereator
+
+
+}**/
